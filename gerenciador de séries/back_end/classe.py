69a493115a55eae04bfcfc9c915e180929997f45
manuzika <roepckemanoela@gmail.com>
2020-06-24 11:06:15 -0300

    cinco instâncias adicionadas

--- gerenciador de séries/back_end/classe.py
+++ gerenciador de séries/back_end/classe.py
@@ -6,15 +6,20 @@ class Serie(db.Model):
     nome = db.Column(db.String(254))
     temporada = db.Column(db.Integer)
     genero = db.Column(db.String(254))
+    status = db.Column(db.String(254))
+    classificacao_indicativa = db.Column(db.Integer)
 
     def __str__(self):
-        return str(self.id)+") "+ self.nome + ", " +\
-            str(self.temporada) + ", " + self.genero
+        return str(self.id)+") " + self.nome + ", " +\
+            str(self.temporada) + ", " + self.genero + ", " +\
+            self.status + ", " + str(self.classificacao_indicativa)
 
     def json(self):
         return json.dumps({
             "id": self.id,
             "nome": self.nome,
             "número de temporadas": self.temporada,
-            "gênero": self.genero
+            "gênero": self.genero,
+            "status": self.status,
+            "classificação indicativa": self.classificacao_indicativa
         })
--- gerenciador de séries/back_end/teste.py
+++ gerenciador de séries/back_end/teste.py
@@ -9,13 +9,30 @@ if __name__ == "__main__":
     db.create_all()
 
     s1 = Serie(nome = "Control Z", temporada = 1, 
-    genero = "Drama adolescente")
+    genero = "Drama Adolescente", status = "Em produção", 
+    classificacao_indicativa = 18)
     s2 = Serie(nome = "V Wars", temporada = 1, 
-    genero = "Terror e Ficção Científica")        
-    
+    genero = "Terror e Ficção Científica", status = "Cancelada",
+    classificacao_indicativa = 16)
+    s3 = Serie(nome = "The Vampire Diaries", temporada = 8, 
+    genero = "Drama, Fantasia e Romance", status = "Encerrada",
+    classificacao_indicativa = 14)
+    s4 = Serie(nome = "Grey's Anatomy", temporada = 16, 
+    genero = "Drama Médico", status = "Em produção",
+    classificacao_indicativa = 14)
+    s5 = Serie(nome = "Blindspot", temporada = 5, 
+    genero = "Ficção Policial", status = "Encerrada",
+    classificacao_indicativa = 14)  
+
     db.session.add(s1)
     db.session.add(s2)
+    db.session.add(s3)
+    db.session.add(s4)
+    db.session.add(s5)
     db.session.commit()
     
     print(s1.json())
     print(s2.json())
+    print(s3.json())
+    print(s4.json())
+    print(s5.json())
